060f7a11f4920c1ce724234b31952110646f3904
Tshidiso10 <tshidisomakinta@gmail.com>
2023-02-08 17:51:35 -0800

    My shell basics project

---
+++ 0x00-shell_basics/1-listit.sh
@@ -0,0 +1,19 @@
+# !/bin/bash
+
+echo "alx-system_engineering-devops"
+
+read dir
+
+if[ -d $dir]
+
+then
+
+	echo "list of files in the directory"
+
+	ls –l $dir|egrep ‘^d’
+
+else
+
+	echo "enter proper directory name"
+
+fi
